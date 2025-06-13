546373803a750298f5662394bb6b10b647c9409c
Cesar Augusto <cesar.dionysio@gmail.com>
2025-06-13 13:15:58 -0300

    site_Lovable_manus

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(async ({ mode }) => {
+  let componentTaggerPlugin = null;
+
+  // Evita erro por causa do módulo ESM
+  if (mode === 'development') {
+    const mod = await import("lovable-tagger");
+    componentTaggerPlugin = mod.componentTagger();
+  }
+
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      componentTaggerPlugin,
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
